e04cacd8e72602ce6c025cd5f441557cdf4a73b0
Jens Münker <jens.muen@web.de>
2023-09-27 11:20:47 +0100

    ADD: Main Activity

---
+++ app/src/main/java/com/frosch2010/lifestyle_scoring_app/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.frosch2010.lifestyle_scoring_app.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.frosch2010.lifestyle_scoring_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
